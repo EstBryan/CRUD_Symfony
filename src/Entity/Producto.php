1a75846f63ffe44845d680e41b1af92058ec63c2
Bryan Estrada <est.armando42@gmail.com>
2022-11-15 20:18:05 -0600

    Inicio del proyecto

---
+++ src/Entity/Producto.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Producto
+ *
+ * @ORM\Table(name="producto")
+ * @ORM\Entity
+ */
+class Producto
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_producto", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idProducto;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=200, nullable=false)
+     */
+    private $descripcion;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="precio", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $precio;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="existencias", type="integer", nullable=false)
+     */
+    private $existencias;
+
+    public function getIdProducto(): ?int
+    {
+        return $this->idProducto;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getExistencias(): ?int
+    {
+        return $this->existencias;
+    }
+
+    public function setExistencias(int $existencias): self
+    {
+        $this->existencias = $existencias;
+
+        return $this;
+    }
+
+
+}
